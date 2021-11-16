546a4d165d66d74c699a618d16d0aa1ce15c8861
LucFabresse <luc.fabresse@gmail.com>
2021-11-16 14:25:34 +0100

    non-root user in container

--- public/php/test-pdo/config.php
+++ public/php/test-pdo/config.php
@@ -1,8 +1,11 @@
 <?php
 
 // define('DB_HOST','127.0.0.1');
+// define('DB_PORT',5306);
+
 define('DB_HOST','mysql');
 define('DB_PORT',3306);
+
 define('DB_DATABASE','test');
 define('DB_USERNAME','mysql');
 define('DB_PASSWORD','mysql');
